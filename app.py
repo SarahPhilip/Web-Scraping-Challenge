efea83db782b4ebb7b43ffa98f84808600bbb745
Shameetha <shameetha@gmail.com>
2018-09-24 22:57:32 -0700

    Final

--- app.py
+++ app.py
@@ -6,22 +6,6 @@ app = Flask(__name__)
 
 client = pymongo.MongoClient('mongodb://localhost:27017')
 
-
-# @app.route('/')
-# def index():
-#     mars = client.db.mars.find_one()
-#     return render_template('index.html', mars=mars)
-
-# @app.route('/scrape')
-# def scrape():
-#     mars = client.db.mars
-#     data = scrape_mars.scrape()
-#     mars.update({}, data, upsert=True)
-#     return redirect("http://localhost:5000/", code=302)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 db = client.mars_db
 collection = db.data
 
@@ -35,20 +19,9 @@ def home():
 def scrape():
     mars_info = scrape_mars.scrape()
 
-    # # Combine results into one dictionary
-    # mars = {
-    #     'news_title': mars_info['news_title'],
-    #     'news_p': mars_info['news_p'],
-    #     # 'featured_image_url': mars_info['featured_image_url'],
-    #     # 'mars_weather': mars_info['mars_weather'],
-    #     # 'tables': mars_info['tables'],
-    #     # 'hem_img_urls': mars_info['hem_img_urls'],
-        
-    # }
-
     # # Insert forecast into database
-    # collection.insert_one(mars_info)
-    collection.update({}, mars_info, upsert=True)
+    collection.drop()
+    collection.insert_one(mars_info)
     # Redirect back to home page
     return redirect('/', code=302)
 
